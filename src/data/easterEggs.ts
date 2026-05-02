8162d7d01a19550cdcbe1275701e1547e56470bf
Avraham Lashansky <avrahamking@gmail.com>
2026-05-02 13:57:52 +0300

    add easter egg vellu

--- src/data/easterEggs.ts
+++ src/data/easterEggs.ts
@@ -2,10 +2,8 @@ interface EasterEggName {
     [name: string]: string
 }
 export const easterEggNames: EasterEggName = {
-    "michael guthrie" : "G.O.A.T",
-    "royi tsuk" : "Alofsrael(2025)",
-    "scott skelton" : "Mr Double ABCDEFUCKYEAH",
-    
-
-
+    "michael guthrie": "G.O.A.T",
+    "royi tsuk": "Alofsrael(2025)",
+    "scott skelton": "Mr Double ABCDEFUCKYEAH",
+    "vellu saarela": "King of Tricking"
 }
